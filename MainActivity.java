1c72a3e89d1f792300574d2a8713257503c4cac0
JG-333 <130273273+JG-333@users.noreply.github.com>
2023-08-01 03:13:57 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,140 +1,145 @@
-package com.example.calculator;
+package com.example.mychatapptutorial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firestore.v1.TargetOrBuilder;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView workingsTV;
-    TextView resultsTV;
 
-    String workings = "";
-    String formula = "";
-    String tempFormula = "";
+
+    EditText mgetphonenumber;
+    android.widget.Button msendotp;
+    CountryCodePicker mcountrycodepicker;
+    String countrycode;
+    String phonenumber;
+
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofmain;
+
+
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String codesent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initTextViews();
+        mcountrycodepicker=findViewById(R.id.countrycodepicker);
+        msendotp=findViewById(R.id.sendotpbutton);
+        mgetphonenumber=findViewById(R.id.getphonenumber);
+        mprogressbarofmain=findViewById(R.id.progressbarofmain);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
+
+        mcountrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
+            }
+        });
+
+        msendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number;
+                number=mgetphonenumber.getText().toString();
+                if(number.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Please Enter YOur number",Toast.LENGTH_SHORT).show();
+                }
+                else if(number.length()<10)
+                {
+                    Toast.makeText(getApplicationContext(),"Please Enter correct number",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    mprogressbarofmain.setVisibility(View.VISIBLE);
+                    phonenumber=countrycode+number;
+
+                    PhoneAuthOptions options=PhoneAuthOptions.newBuilder(firebaseAuth)
+                            .setPhoneNumber(phonenumber)
+                            .setTimeout(60L, TimeUnit.SECONDS)
+                            .setActivity(MainActivity.this)
+                            .setCallbacks(mCallbacks)
+                            .build();
+
+
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+
+
+
+                }
+
+
+            }
+        });
+
+
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                //how to automatically fetch code here
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(),"OTP is Sent",Toast.LENGTH_SHORT).show();
+                mprogressbarofmain.setVisibility(View.INVISIBLE);
+                codesent=s;
+                Intent intent=new Intent(MainActivity.this,otpAuthentication.class);
+                intent.putExtra("otp",codesent);
+                startActivity(intent);
+            }
+        };
+
+
+
     }
 
-    private void initTextViews()
-    {
-        workingsTV = (TextView)findViewById(R.id.workingsTextView);
-        resultsTV = (TextView)findViewById(R.id.resultsTextView);
-    }
 
-    private void setWorkings(String givenValue)
-    {
-        workings = workings + givenValue;
-        workingsTV.setText(workings);
-    }
-
-    public void equalsOnClick(View view) {
-        Double result = null;
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
-        checkForPowerOf();
-
-        try {
-            result = (double)engine.eval(formula);
-        }
-        catch (ScriptException e)
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null)
         {
-            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
-        }
-        if(result != null) {
-            resultsTV.setText(String.valueOf(result.doubleValue()));
-        }
-    }
-
-    private void checkForPowerOf() {
-        ArrayList<Integer> indexOfPowers = new ArrayList<>();
-        for (int i = 0; i < workings.length(); i++) {
-            if (workings.charAt(i) == '^')
-                indexOfPowers.add(i);
-        }
-        formula = workings;
-        tempFormula = workings;
-        for (Integer index : indexOfPowers) {
-            changeFormula(index);
-        }
-        formula = tempFormula;
-    }
-
-    private void changeFormula(Integer index) {
-        String numberLeft = "";
-        String numberRight = "";
-
-        for(int i = index + 1; i< workings.length(); i++)
-        {
-            if(isNumeric(workings.charAt(i)))
-                numberRight = numberRight + workings.charAt(i);
-            else
-                break;
-        }
-        for(int i = index - 1; i >= 0; i--)
-        {
-            if(isNumeric(workings.charAt(i)))
-                numberLeft = numberLeft + workings.charAt(i);
-            else
-                break;
+            Intent intent=new Intent(MainActivity.this,chatActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
         }
 
-        String original = numberLeft + "^" + numberRight;
-        String changed = "Math.pow("+numberLeft+","+numberRight+")";
-        tempFormula = tempFormula.replace(original,changed);
-    }
-    private boolean isNumeric(char c) {
-        if ((c <= '9' && c >= '0') || c == '.')
-            return true;
 
-        return false;
-    }
-    public void clearOnClick(View view)
-    {
-        workingsTV.setText("");
-        workings = "";
-        resultsTV.setText("");
-        leftBracket = true;
-    }
 
-    boolean leftBracket = true;
 
-    public void bracketsOnClick(View view) {
-        if (leftBracket) {
-            setWorkings("(");
-            leftBracket = false;
-        } else {
-            setWorkings(")");
-            leftBracket = true;
-        }
-    }
 
-    public void powerOfOnclick(View view)
-    {
-        setWorkings("^");
-    }
 
-    public void divisionOnclick(View view)
-    {
-        setWorkings("/");
-    }
-
-    public void zeroOnclick(View view)
-    {
-        setWorkings("0");
-    }
-
-    public void oneOnclick(View view)
-    {
-        setWorkings("1");
-    }
-
-    public void twoOnclick(View view)
-    {
-        setWorkings("2");
     }
 }
